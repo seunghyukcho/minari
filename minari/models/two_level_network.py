52f2b1f8bd64af74be3c169e691bc22529855b3b
shhj1998 <shhj1998@postech.ac.kr>
2019-08-06 18:28:32 +0900

    simple two level network

---
+++ minari/models/two_level_network.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as torch_function
+
+
+class TwoLevelNetwork(nn.Module):
+    def __init__(self, input_size1, input_size2):
+        super(TwoLevelNetwork, self).__init__()
+
+        self.fc1 = nn.Linear(input_size1, 1)
+        self.fc2 = nn.Linear(input_size2 + 1, 1)
+
+    def forward(self, x1, x2):
+        x1 = torch_function.leaky_relu(self.fc1(x1))
+        x2 = torch.cat((x1, x2), 1)
+        x2 = torch_funciton.leaky_relu(self.fc2(x2))
+
+        return x2
