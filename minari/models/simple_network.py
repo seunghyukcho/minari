26de149d82b7c577ca80dd1bef114a7cf006c753
shhj1998 <shhj1998@postech.ac.kr>
2019-08-09 22:25:04 +0900

    snn multi layer experiment

--- minari/models/simple_network.py
+++ minari/models/simple_network.py
@@ -7,13 +7,25 @@ class SimpleNetwork(nn.Module):
         super(SimpleNetwork, self).__init__()
 
         self.fc1 = nn.Linear(input_size, 400)
-        self.fc2 = nn.Linear(400, 1)
+        self.fc2 = nn.Linear(400, 400)
+        self.fc3 = nn.Linear(400, 400)
+        self.fc4 = nn.Linear(400, 400)
+        self.fc5 = nn.Linear(400, 400)
+        self.fc6 = nn.Linear(400, 1)
 
         nn.init.xavier_uniform_(self.fc1.weight)
         nn.init.xavier_uniform_(self.fc2.weight)
+        nn.init.xavier_uniform_(self.fc3.weight)
+        nn.init.xavier_uniform_(self.fc4.weight)
+        nn.init.xavier_uniform_(self.fc5.weight)
+        nn.init.xavier_uniform_(self.fc6.weight)
 
     def forward(self, x):
         x = torch_function.leaky_relu(self.fc1(x))
         x = torch_function.leaky_relu(self.fc2(x))
+        x = torch_function.leaky_relu(self.fc3(x))
+        x = torch_function.leaky_relu(self.fc4(x))
+        x = torch_function.leaky_relu(self.fc5(x))
+        x = torch_function.leaky_relu(self.fc6(x))
 
         return x
